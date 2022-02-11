0592864bbbd7bae0930c5766a81ce17f2e430c94
Imran Zahid <imranzahid@gmail.com>
2022-02-11 15:00:00 +0500

    Initial release

---
+++ Casks/temurin11lts.rb
@@ -0,0 +1,27 @@
+cask "temurin11lts" do
+  version "11.0.14.1,1"
+  sha256 "1c165d086d89cc540d01efbf60fcfe9ead6a7ede64d6d3a75ff6e2a8c94bd8f4"
+
+  url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
+      verified: "github.com/adoptium/"
+  name "Eclipse Temurin 11"
+  desc "JDK from the Eclipse Foundation (Adoptium)"
+  homepage "https://adoptium.net/"
+
+  livecheck do
+    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    strategy :page_match do |page|
+      JSON.parse(page).map do |release|
+        match = release["release_name"].match(/^jdk-(\d+(?:\.\d+)+)\+(\d+(?:\.\d+)*)$/i)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end.compact
+    end
+  end
+
+  pkg "OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg"
+
+  uninstall pkgutil: "net.temurin.#{version.major}.jdk"
+end
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-java
+Maintain Java LTS Release
+
+Maintian LTS releases. When a new Java version is released official cask will install that, even if the new version is non LTS.
+This `tap` makes sure only LTS is maintained and installed.
+
+Currently only `11` and `17` casks are enabled
+
