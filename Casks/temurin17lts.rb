3d9742fe5da3c79775d6e3ceab21031d36c63e04
Imran Zahid <imranzahid@gmail.com>
2022-09-08 16:41:02 +0500

    Update 17 version

--- Casks/temurin17lts.rb
+++ Casks/temurin17lts.rb
@@ -1,12 +1,11 @@
 cask "temurin17lts" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  version "17.0.4.1,1"
 
   if Hardware::CPU.intel?
-    sha256 "70afd9b4f76d69a5f9ab3f740a9289f41d23558f02b133e684c80380c265455f"
-	version "17.0.3,7"
+    sha256 "ac21a5a87f7cfa00212ab7c41f7eb80ca33640d83b63ad850be811c24095d61a"
   else
-    sha256 "0904c11eb5f7b53aac53e6279992ee2a900c9517b26659dee3704b2d11c149cd"
-	version "17.0.2,8"
+    sha256 "3a976943a9e6a635e68e2b06bd093fc096aad9f5894acda673d3bea0cb3a6f38"
   end
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
